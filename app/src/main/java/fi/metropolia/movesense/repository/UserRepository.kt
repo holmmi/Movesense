ecd6ca4fcffa53990feb8a65cbaf29594df19fb9
tiituste <tiitus.telke@metropolia.fi>
2022-04-28 22:58:57 +0300

    Add features for getting firebase id token

--- app/src/main/java/fi/metropolia/movesense/repository/UserRepository.kt
+++ app/src/main/java/fi/metropolia/movesense/repository/UserRepository.kt
@@ -7,41 +7,59 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import fi.metropolia.movesense.api.UserApi
 import fi.metropolia.movesense.model.api.LoginRequest
 import fi.metropolia.movesense.model.api.RegisterRequest
 import fi.metropolia.movesense.model.api.RegisterResponse
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
 import retrofit2.Response
 import retrofit2.await
 import retrofit2.awaitResponse
-import java.lang.Exception
+
 
 class UserRepository(val context: Context) {
+    private var auth: FirebaseAuth = FirebaseAuth.getInstance()
     private val userService = UserApi.service
+
     val getUserToken: Flow<String?> = context.dataStore.data
         .map { preferences ->
             // No type safety.
             preferences[TOKEN_KEY] ?: ""
         }
 
-    suspend fun saveUserToken(token: String?) = token?.let {
+    private suspend fun saveUserToken(token: String?) = token?.let {
         context.dataStore.edit { preferences ->
             preferences[TOKEN_KEY] = token
         }
     }
 
+    private fun getFireBaseToken(): String? {
+        val mUser: FirebaseUser? = auth.currentUser
+        var idToken: String? = null
+        mUser?.getIdToken(true)?.addOnCompleteListener { task ->
+            idToken = if (task.isSuccessful) {
+                task.result.token
+            } else {
+                // Handle error -> task.getException();
+                null
+            }
+        }
+        return idToken
+    }
+
     suspend fun login(loginRequest: LoginRequest) =
         withContext(Dispatchers.IO) {
             try {
-                saveUserToken(
-                    userService.loginUser(loginRequest)?.await()?.token
-                )
+                val token = userService.loginUser(loginRequest)?.await()?.token
+                saveUserToken(token)
+                token?.let {
+                    auth.signInWithCustomToken(it)
+                }
             } catch (e: Exception) {
                 Log.e(TAG, "login error: ${e.localizedMessage}")
                 null
@@ -66,7 +84,7 @@ class UserRepository(val context: Context) {
 
     suspend fun getDetails() = withContext(Dispatchers.IO) {
         try {
-            val token: String? = getUserToken.first()
+            val token: String? = getFireBaseToken()
             userService.userDetails(token).await()
         } catch (e: Exception) {
             Log.e(TAG, "getDetails error: ${e.localizedMessage}")
